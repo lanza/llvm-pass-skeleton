29c749d1f5d7a62c2969c277692b8bd1c1e57539
Nathan Lanza <nathan@lanza.io>
2021-10-27 22:52:32 -0400

    Play with sample c file

--- a.c
+++ a.c
@@ -1,8 +1,18 @@
-// godbolt: -Xclang -load -Xclang build/skeleton/libSkeletonPass.so -S -emit-llvm -flegacy-pass-manager -O3
+// godbolt: -Xclang -load -Xclang build/skeleton/libSkeletonPass.so -S
+// -emit-llvm -flegacy-pass-manager -O3
 
-int puts(char const*);
+int puts(char const *);
 
-__attribute__((annotate("invoke_me","muffin")))
+#define JNI(javaSymbol, nativeSymbol)                                          \
+  __attribute__((annotate("JNIRegistration", javaSymbol, nativeSymbol)))
+
+#define JNIFunc(returnType, name, javaSymbol, ...)                             \
+  __attribute__((annotate("JNIRegistration", javaSymbol, #name))) returnType   \
+  name(__VA_ARGS__)
+
+JNIFunc(int, doStuff, "muffin", int q) { return 4; }
+
+JNI("muffin", "doThing")
 int doThing(int a) {
   puts("I got called!");
   return a;
@@ -12,4 +22,3 @@ int main(int argc, char const **argv) {
   int a = 42 + argc;
   return a;
 }
-
